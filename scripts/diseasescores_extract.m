de36912e1fc55aedb1f883068aacd302efdef99f
Perry <alistairperry123@gmail.com>
2018-07-31 12:09:16 +0200

    add transform

---
+++ scripts/diseasescores_extract.m
@@ -0,0 +1,33 @@
+function [diseasescores] = diseasescores_extract
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+workingdirectory = pwd;
+files = dir(workingdirectory);
+dirFlags=[files.isdir];
+subFolders=files(dirFlags);
+subFolders(1:2)=[];
+
+for s = 1:length(subFolders)
+    currentSubj= subFolders(s,1).name;
+    currentSubjDir = char([workingdirectory '/' currentSubj]);
+    
+    subjdismean2mm=dlmread([currentSubjDir '/' currentSubj '' '_DBM_dis_mean_2mm.txt']); %Matrix file containing information of each subject
+    dismean2mm(s,1) = subjdismean2mm; %Matlab field representing connectivity matrix
+
+    subjdismean8mm=dlmread([currentSubjDir '/' currentSubj '' '_DBM_dis_mean_8mm.txt']); %Matrix file containing information of each subject
+    dismean8mm(s,1) = subjdismean8mm; %Matlab field representing connectivity matrix
+
+    subjdismed2mm=dlmread([currentSubjDir '/' currentSubj '' '_DBM_dis_med_2mm.txt']); %Matrix file containing information of each subject
+    dismed2mm(s,1) = subjdismed2mm; %Matlab field representing connectivity matrix
+    
+    subjdismed8mm=dlmread([currentSubjDir '/' currentSubj '' '_DBM_dis_med_8mm.txt']); %Matrix file containing information of each subject
+    dismed8mm(s,1) = subjdismed8mm; %Matlab field representing connectivity matrix
+
+end
+
+diseasescores=table(dismean2mm, dismean8mm, dismed2mm, dismed8mm);
+%diseasescores.Properties.VariableNames{'subFolders.name'} = 'Patient';
+
+end
+
