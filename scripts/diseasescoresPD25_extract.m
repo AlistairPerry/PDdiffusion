7268831dd2859ebf7c9814a0ba966015600cb3dc
Perry <alistairperry123@gmail.com>
2019-07-31 14:35:05 +0200

    now includes check for missing

--- scripts/diseasescoresPD25_extract.m
+++ scripts/diseasescoresPD25_extract.m
@@ -12,28 +12,36 @@ for s = 1:length(subFolders)
     currentSubj= subFolders(s,1).name;
     currentSubjDir = char([workingdirectory '/' currentSubj]);
     
-    subjdismean=dlmread([currentSubjDir '/' currentSubj '' '_DBM_dis_mean_PD25gm.txt']); %Matrix file containing information of each subject
-    dismean(s,1) = subjdismean; %Matlab field representing connectivity matrix
-
-    subjdismed=dlmread([currentSubjDir '/' currentSubj '' '_DBM_dis_med_PD25gm.txt']); %Matrix file containing information of each subject
-    dismed(s,1) = subjdismed; %Matlab field representing connectivity matrix
+    %     subjdismean=dlmread([currentSubjDir '/' currentSubj '' '_DBM_dis_mean_PD25gm.txt']); %Matrix file containing information of each subject
+    %     dismean(s,1) = subjdismean; %Matlab field representing connectivity matrix
+    %
+    %     subjdismed=dlmread([currentSubjDir '/' currentSubj '' '_DBM_dis_med_PD25gm.txt']); %Matrix file containing information of each subject
+    %     dismed(s,1) = subjdismed; %Matlab field representing connectivity matrix
     
-    subjdismean2mm=dlmread([currentSubjDir '/' currentSubj '' '_DBM_dis_mean_2mm_PD25gm.txt']); %Matrix file containing information of each subject
-    dismean2mm(s,1) = subjdismean2mm; %Matlab field representing connectivity matrix
-
-    subjdismean8mm=dlmread([currentSubjDir '/' currentSubj '' '_DBM_dis_mean_8mm_PD25gm.txt']); %Matrix file containing information of each subject
-    dismean8mm(s,1) = subjdismean8mm; %Matlab field representing connectivity matrix
-
-    subjdismed2mm=dlmread([currentSubjDir '/' currentSubj '' '_DBM_dis_med_2mm_PD25gm.txt']); %Matrix file containing information of each subject
-    dismed2mm(s,1) = subjdismed2mm; %Matlab field representing connectivity matrix
+    if exist([currentSubjDir '/' currentSubj '' '_DBM_dis_mean_2mm.txt'], 'file') == 2
+        
+        allsubjs{s,1} = currentSubj;
+        
+%         subjdismean2mm=dlmread([currentSubjDir '/' currentSubj '' '_DBM_dis_mean_2mm.txt']); %Matrix file containing information of each subject
+%         dismean2mm(s,1) = subjdismean2mm; %Matlab field representing connectivity matrix
+        
+        subjdismean8mm=dlmread([currentSubjDir '/' currentSubj '' '_DBM_dis_mean_8mm.txt']); %Matrix file containing information of each subject
+        dismean8mm(s,1) = subjdismean8mm; %Matlab field representing connectivity matrix
+        
+        subjdismed2mm=dlmread([currentSubjDir '/' currentSubj '' '_DBM_dis_med_2mm.txt']); %Matrix file containing information of each subject
+        dismed2mm(s,1) = subjdismed2mm; %Matlab field representing connectivity matrix
+        
+        subjdismed8mm=dlmread([currentSubjDir '/' currentSubj '' '_DBM_dis_med_8mm.txt']); %Matrix file containing information of each subject
+        dismed8mm(s,1) = subjdismed8mm; %Matlab field representing connectivity matrix
+        
+    end
     
-    subjdismed8mm=dlmread([currentSubjDir '/' currentSubj '' '_DBM_dis_med_8mm_PD25gm.txt']); %Matrix file containing information of each subject
-    dismed8mm(s,1) = subjdismed8mm; %Matlab field representing connectivity matrix
-
 end
 
-diseasescores=table(subFolders.name, dismean, dismed, dismean2mm, dismean8mm, dismed2mm, dismed8mm);
-diseasescores.Properties.VariableNames{'subFolders.name'} = 'Patient';
+%diseasescores=table(allsubjs, dismean, dismed, dismean2mm, dismean8mm, dismed2mm, dismed8mm);
+
+diseasescores=table(allsubjs, dismean8mm, dismed2mm, dismed8mm);
+diseasescores.Properties.VariableNames{'allsubjs'} = 'Patient';
 
 end
 
